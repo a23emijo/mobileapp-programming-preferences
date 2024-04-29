c1d1718c255f0086eac139c86c06d0beea9182dc
a23emijo <a23emijo@student.his.se>
2024-04-29 11:02:30 +0200

    Added a button that opens the second activity and added texts to show which activity that is currently open

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Variables
+    private Button buttonToSecondActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
+        buttonToSecondActivity = findViewById(R.id.goToSecondActivity); // Finds the to second activity button
 
+        // Sets a OnClickListener for the to second activity button
+        buttonToSecondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Sets the intents
+                startActivity(new Intent(MainActivity.this, SecondActivity.class)); // Starts the activity with the intents
+            }
+        });
+    }
 }
