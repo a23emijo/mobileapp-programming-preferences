6386ca28afdf661984a7971362f9bd5b85df6371
a23emijo <a23emijo@student.his.se>
2024-04-30 12:25:46 +0200

    Added a onResume to make the application save the new text when going back to Main Activity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,12 +13,17 @@ public class MainActivity extends AppCompatActivity {
 
     // Variables
     private Button buttonToSecondActivity;
+    private SharedPreferences myPreferenceRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         buttonToSecondActivity = findViewById(R.id.goToSecondActivity); // Finds the to second activity button
+
+        myPreferenceRef = getSharedPreferences("MyPreferencesName", MODE_PRIVATE);
 
         // Sets a OnClickListener for the to second activity button
         buttonToSecondActivity.setOnClickListener(new View.OnClickListener() {
@@ -27,5 +33,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SecondActivity.class)); // Starts the activity with the intents
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        TextView prefTextRef=new TextView(this);
+        prefTextRef=(TextView)findViewById(R.id.prefText);
+        prefTextRef.setText(myPreferenceRef.getString("MyAppPreferenceString", "No preference found."));
     }
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,13 +1,35 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        myPreferenceRef = getSharedPreferences("MyPreferencesName", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
     }
+    public void savePref(View v){
+        // Get the text
+        EditText newPrefText=new EditText(this);
+        newPrefText=(EditText)findViewById(R.id.settingseditview);
+
+        // Store the new preference
+        myPreferenceEditor.putString("MyAppPreferenceString", newPrefText.getText().toString());
+        myPreferenceEditor.apply();
+
+        // Clear the EditText
+        newPrefText.setText("");
+    }
+
 }
